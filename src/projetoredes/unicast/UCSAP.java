7013bffafb23e7fa45f5c9001519192846fded7d
Joao Victor Lopes Pereira dos Santos <joaovlpss@usp.br>
2025-09-26 08:57:49 -0300

    FIX: Mudado tipo do ID de UCSAP para short

--- src/projetoredes/unicast/UCSAP.java
+++ src/projetoredes/unicast/UCSAP.java
@@ -2,4 +2,4 @@ package projetoredes.unicast;
 
 // A classe 'record' automaticamente gera construtores, métodos 'equals', 
 // 'hashCode' e 'toString' com base nos campos definidos.
-public record UCSAP(int id, String host, int port) {}
+public record UCSAP(short id, String host, int port) {}
