0caa1934bbb2ae0a17a3478adf037008e8dc4710
Joao Victor Lopes <joaovlpssdev@gmail.com>
2025-09-30 20:43:42 -0300

    FEAT: Iniciado app simples para demonstrar UnicastProtocol.

---
+++ src/projetoredes/app/SimpleUnicastApp.java
@@ -0,0 +1,54 @@
+package projetoredes.app;
+
+import projetoredes.unicast.UnicastServiceUserInterface;
+
+import java.util.Scanner;
+
+import projetoredes.unicast.UnicastProtocol;
+
+
+public class SimpleUnicastApp implements UnicastServiceUserInterface, AutoCloseable {
+
+    private final UnicastProtocol protocol;
+    private final short selfId;
+
+    public SimpleUnicastApp(short selfId, String configPath) throws Exception {
+        this.selfId = selfId;
+        // Iniciar a thread receptora
+        this.protocol = new UnicastProtocol(this, selfId, configPath);
+
+        System.out.println("Aplicação iniciada com ID " + selfId);
+    }
+    
+    @Override
+    public void UPDataInd(short sourceId, String data) {
+        // Esse e um app simples de teste, entao vamos 
+        // apenas printar a mensagem para a tela.
+        System.out.println("\r[Mensagem recebida de ID " + sourceId + "]: " + data);
+        System.out.print("> ");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Comandos disponíveis:");
+        System.out.println("  send <id_destino> <mensagem>   - Envia uma mensagem");
+        System.out.println("  exit                           - Fecha a aplicação");
+        System.out.println("---------------------------------------------------------");
+
+        while(true) {
+            System.out.print("> ");
+            String line = scanner.nextLine();
+
+            if(line.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            String[] parts = line.split(" ", 3);
+
+            if(parts.length == 3 && parts[0].equalsIgnoreCase("send")) {
+                // TODO: Implementar envio de mensagem.
+            }
+        }
+    }
+}
